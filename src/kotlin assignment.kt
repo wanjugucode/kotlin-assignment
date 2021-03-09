9b03182e070a32184203884c288a87ba4ec8d99b
wanjugucode <karobiaanastasia.com>
2021-03-09 23:26:38 +0300

    first commit

---
+++ src/kotlin assignment.kt
@@ -0,0 +1,37 @@
+fun main() {
+    school()
+    info("Annah",24)
+    fan()
+    name("Annah","Annah")
+}
+fun school(){
+    var s="akirchix"
+    println(s[0])
+    println(s[3])
+    println(s[4])
+}
+fun info(name:String,age:Int){
+    println("Hi my name is $name is am $age year old")
+
+}
+fun fan(){
+    var s="i found the tressure"
+    println(s.length)
+
+}
+fun name (name: String,name2:String){
+    if (name==name2){
+        println("Thats me")
+
+}
+ else {
+     println("I dont know who that is")
+
+}
+
+
+
+
+
+}
+
